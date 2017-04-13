678c0cedbec5ec6802deb83dc8434669221317f3
Shivani chaudhary <c.rinku.rc@gmail.com>
2017-04-13 05:25:25 +0000

    add app.js

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect(process.env.MONGODB_URI);
+//mongodb://shivani:<dbpassword>@ds157980.mlab.com:57980/yelp_camp    
+    
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
